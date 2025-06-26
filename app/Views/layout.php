91eb95cbeb7913eb889303cec8e02e03574c5b73
Akmal wizdom <faiqihya@gmail.com>
2025-06-27 05:54:53 +0700

    merapikan layout

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -12,20 +12,22 @@ if (uri_string() != "") {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>- Toko - <?php echo $hlm ?></title>
+
   <!-- plugins:css -->
   <link rel="stylesheet" href="<?= base_url() ?>corona/dist/assets/vendors/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>corona/dist/assets/vendors/ti-icons/css/themify-icons.css">
   <link rel="stylesheet" href="<?= base_url() ?>corona/dist/assets/vendors/css/vendor.bundle.base.css">
   <link rel="stylesheet" href="<?= base_url() ?>corona/dist/assets/vendors/font-awesome/css/font-awesome.min.css">
-  <!-- endinject -->
+
   <!-- Plugin css for this page -->
   <link rel="stylesheet" href="<?= base_url() ?>corona/dist/assets/vendors/jvectormap/jquery-jvectormap.css">
   <link rel="stylesheet" href="<?= base_url() ?>corona/dist/assets/vendors/flag-icon-css/css/flag-icons.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>corona/dist/assets/vendors/owl-carousel-2/owl.carousel.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>corona/dist/assets/vendors/owl-carousel-2/owl.theme.default.min.css">
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <!-- endinject -->
+
+  <!-- Select2 CSS - DIPINDAHKAN KE HEAD -->
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
   <!-- Layout styles -->
   <link rel="stylesheet" href="<?= base_url() ?>corona/dist/assets/css/style.css">
   <!-- End layout styles -->
@@ -52,15 +54,10 @@ if (uri_string() != "") {
   </div>
   <!-- container-scroller -->
 
-  <!-- jQuery -->
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-  <!-- Select2 -->
-  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
   <!-- plugins:js -->
   <script src="<?= base_url() ?>corona/dist/assets/vendors/js/vendor.bundle.base.js"></script>
   <!-- endinject -->
+
   <!-- Plugin js for this page -->
   <script src="<?= base_url() ?>corona/dist/assets/vendors/chart.js/chart.umd.js"></script>
   <script src="<?= base_url() ?>corona/dist/assets/vendors/progressbar.js/progressbar.min.js"></script>
@@ -69,17 +66,24 @@ if (uri_string() != "") {
   <script src="<?= base_url() ?>corona/dist/assets/vendors/owl-carousel-2/owl.carousel.min.js"></script>
   <script src="<?= base_url() ?>corona/dist/assets/js/jquery.cookie.js" type="text/javascript"></script>
   <!-- End plugin js for this page -->
+
+  <!-- jQuery dan Select2 JS - SETELAH VENDOR JS -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
   <!-- inject:js -->
   <script src="<?= base_url() ?>corona/dist/assets/js/off-canvas.js"></script>
   <script src="<?= base_url() ?>corona/dist/assets/js/misc.js"></script>
   <script src="<?= base_url() ?>corona/dist/assets/js/settings.js"></script>
   <script src="<?= base_url() ?>corona/dist/assets/js/todolist.js"></script>
   <!-- endinject -->
+
   <!-- Custom js for this page -->
   <script src="<?= base_url() ?>corona/dist/assets/js/proBanner.js"></script>
   <script src="<?= base_url() ?>corona/dist/assets/js/dashboard.js"></script>
   <!-- End custom js for this page -->
-  <?= $this->renderSection('script') ?> 
+
+  <?= $this->renderSection('script') ?>
 </body>
 
 </html>
