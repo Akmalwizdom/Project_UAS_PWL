1d4ccae859fd10d9b094675d9cb1bcc3e0c4bd07
Akmal wizdom <faiqihya@gmail.com>
2025-06-26 09:00:00 +0700

    update image

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -13,7 +13,7 @@
                             </ol>
                         </nav>
                         <h3 class="font-weight-bold mb-0">Selamat Datang di Toko Kami!</h3>
-                        <p class="mb-0">Temukan laptop terbaik untuk kebutuhan Anda.</p>
+                        <p class="mb-0">Temukan jam tangan terbaik untuk kebutuhan Anda.</p>
                     </div>
                 </div>
             </div>
@@ -34,78 +34,20 @@
                         </div>
 
                         <div class="row">
-                            <div class="col-md-4 grid-margin stretch-card">
-                                <div class="card">
-                                    <div class="card-body text-center">
-                                        <img src="assets/images/laptop1.jpg" alt="ASUS TUF A15" class="img-fluid mb-3" style="max-height: 200px; border-radius: 8px;">
-                                        <h5 class="card-title">ASUS TUF A15 FA506NF</h5>
-                                        <h4 class="text-primary font-weight-bold">Rp 10.999.000</h4>
-                                        <button class="btn btn-primary btn-rounded">
-                                            <i class="mdi mdi-cart-plus me-2"></i>Tambah ke Keranjang
-                                        </button>
+                            <?php foreach ($product as $key => $item) : ?>
+                                <div class="col-md-4 grid-margin stretch-card">
+                                    <div class="card">
+                                        <div class="card-body text-center">
+                                            <img src="<?php echo base_url() . 'img/' . $item['foto']; ?>" alt="<?php echo $item['nama']; ?>" class="img-fluid mb-3" style="max-height: 200px; border-radius: 8px;">
+                                            <h5 class="card-title"><?php echo $item['nama']; ?></h5>
+                                            <h4 class="text-primary font-weight-bold">Rp <?php echo number_format($item['harga'], 0, ',', '.'); ?></h4>
+                                            <button class="btn btn-primary btn-rounded">
+                                                <i class="mdi mdi-cart-plus me-2"></i>Tambah ke Keranjang
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-4 grid-margin stretch-card">
-                                <div class="card">
-                                    <div class="card-body text-center">
-                                        <img src="assets/images/laptop2.jpg" alt="Asus Vivobook" class="img-fluid mb-3" style="max-height: 200px; border-radius: 8px;">
-                                        <h5 class="card-title">Asus Vivobook 14 A1404ZA</h5>
-                                        <h4 class="text-primary font-weight-bold">Rp 6.899.000</h4>
-                                        <button class="btn btn-primary btn-rounded">
-                                            <i class="mdi mdi-cart-plus me-2"></i>Tambah ke Keranjang
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4 grid-margin stretch-card">
-                                <div class="card">
-                                    <div class="card-body text-center">
-                                        <img src="assets/images/laptop3.jpg" alt="Lenovo IdeaPad" class="img-fluid mb-3" style="max-height: 200px; border-radius: 8px;">
-                                        <h5 class="card-title">Lenovo IdeaPad Slim 3</h5>
-                                        <h4 class="text-primary font-weight-bold">Rp 6.299.000</h4>
-                                        <button class="btn btn-primary btn-rounded">
-                                            <i class="mdi mdi-cart-plus me-2"></i>Tambah ke Keranjang
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4 grid-margin stretch-card">
-                                <div class="card">
-                                    <div class="card-body text-center">
-                                        <img src="assets/images/laptop3.jpg" alt="Lenovo IdeaPad" class="img-fluid mb-3" style="max-height: 200px; border-radius: 8px;">
-                                        <h5 class="card-title">Lenovo IdeaPad Slim 3</h5>
-                                        <h4 class="text-primary font-weight-bold">Rp 6.299.000</h4>
-                                        <button class="btn btn-primary btn-rounded">
-                                            <i class="mdi mdi-cart-plus me-2"></i>Tambah ke Keranjang
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4 grid-margin stretch-card">
-                                <div class="card">
-                                    <div class="card-body text-center">
-                                        <img src="assets/images/laptop3.jpg" alt="Lenovo IdeaPad" class="img-fluid mb-3" style="max-height: 200px; border-radius: 8px;">
-                                        <h5 class="card-title">Lenovo IdeaPad Slim 3</h5>
-                                        <h4 class="text-primary font-weight-bold">Rp 6.299.000</h4>
-                                        <button class="btn btn-primary btn-rounded">
-                                            <i class="mdi mdi-cart-plus me-2"></i>Tambah ke Keranjang
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4 grid-margin stretch-card">
-                                <div class="card">
-                                    <div class="card-body text-center">
-                                        <img src="assets/images/laptop3.jpg" alt="Lenovo IdeaPad" class="img-fluid mb-3" style="max-height: 200px; border-radius: 8px;">
-                                        <h5 class="card-title">Lenovo IdeaPad Slim 3</h5>
-                                        <h4 class="text-primary font-weight-bold">Rp 6.299.000</h4>
-                                        <button class="btn btn-primary btn-rounded">
-                                            <i class="mdi mdi-cart-plus me-2"></i>Tambah ke Keranjang
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
+                            <?php endforeach; ?>
                         </div>
                     </div>
                 </div>
